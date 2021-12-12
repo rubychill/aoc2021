a5d5a7e05ecda25c75d20bef3d77f527c4456dd6
Ruby Hill <ruby@thatchill.com>
2021-12-12 10:17:12 +1000

    day1 rewritten with coroutines

--- day01/main.c
+++ day01/main.c
@@ -3,16 +3,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "../util/util.h"
+#include "../util/cute_coroutine.h"
 
 const int lines = 2000;
 const int lineLength = 6;
+// const int lines = 10;
+// const int lineLength = 5;
 
 int main() {
   int* depths = malloc(sizeof(int)*lines);
   loadInputInts("input.txt", lines, lineLength, depths);
   //part1(depths);
-  part2(depths);
+  //part2(depths);
+  part1co(depths);
   free(depths);
   return 0;
 }
@@ -47,4 +52,49 @@ void part2(int* depths) {
   part1(groupDepths);
 
   free(groupDepths);
+}
+
+void part1co(int* depths) {
+  coroutine_t co;
+  coroutine_init(&co);
+
+  int* result = calloc(sizeof(int), 1);
+  int donePercent = 0;
+  while(donePercent < 100) {
+    donePercent = doPart1(&co, depths, result);
+    printf("done: %d\n", donePercent);
+  }
+  printf("result: %d\n", *result);
+}
+
+int doPart1(coroutine_t* co, int* depths, int* result) {
+  int* previousPtr = (int*)coroutine_local_var(co, sizeof(int));
+  int* increasesPtr = (int*)coroutine_local_var(co, sizeof(int));
+  int* donePercentPtr = (int*)coroutine_local_var(co, sizeof(int));
+  int previous = *previousPtr;
+  int increases = *increasesPtr;
+  int donePercent = *donePercentPtr;
+  COROUTINE_START(co);
+  previous = -1;
+  increases = 0;
+
+  for (int i = 0; i < lines; i++) {
+    int depth = depths[i];
+    if (previous != -1 && depth > previous) {
+      increases++;
+    }
+    previous = depth;
+    donePercent = ((i + 1) * 100) / lines;
+    if (donePercent < 100) {
+      COROUTINE_YIELD(co);
+    }
+  }
+
+  *result = increases;
+
+  COROUTINE_END(co);
+  *previousPtr = previous;
+  *increasesPtr = increases;
+  *donePercentPtr = donePercent;
+  return donePercent;
 }
--- day01/main.h
+++ day01/main.h
@@ -1,10 +1,12 @@
 #ifndef DAY1_H
 #define DAY1_H
 
+#include "../util/cute_coroutine.h"
+
 void day1();
 void part1(int* depths);
 void part2(int* depths);
-
-int day1golf(int* d);
+void part1co(int* depths);
+int doPart1(coroutine_t* co, int* depths, int* result);
 
 #endif
